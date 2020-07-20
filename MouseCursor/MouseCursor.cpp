5f72e1677911067a77cafd98c35a59b3fa9b680b
NithenV <vladkastom97@mail.ru>
2020-07-20 16:15:37 +0300

    Добавьте файлы проекта.

---
+++ MouseCursor/MouseCursor.cpp
@@ -0,0 +1,34 @@
+﻿#include "iostream"
+#include <Windows.h>
+
+void MoveCursor()
+{
+    static bool EnableMove = true;
+    POINT cc;
+    while (true)
+    {
+        int ms = 0;
+        while (GetAsyncKeyState(VK_LCONTROL) && EnableMove && ms < 1500) // Движение курсора по оси X
+        {
+            GetCursorPos(&cc);
+            SetCursorPos(cc.x, cc.y + 1);
+            Sleep(1);
+            ms++;
+        }
+        ms = 0;
+
+        while (GetAsyncKeyState(VK_LCONTROL) && EnableMove && ms < 1500) // Движение курсора по отрицательной оси Y
+        {
+            GetCursorPos(&cc);
+            SetCursorPos(cc.x + 1, cc.y);
+            Sleep(1);
+            ms++;
+        }
+        ms = 0;
+    }
+}
+
+int main()
+{
+    MoveCursor();
+}
